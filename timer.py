7e01ec4c298b36806f37e7430550abc6b9cf7cc0
Priates of Zimsey <realcoder2023@gmail.com>
2025-10-08 11:51:02 -0400

    added to repo and commited to repo on github

---
+++ timer.py
@@ -0,0 +1,28 @@
+﻿import asyncio
+import time
+from typing import TypeAlias
+
+TimerTyped: TypeAlias = int
+
+def get_time():
+    return time.sleep(1)
+
+async def get_Timer() -> None:
+    timer = TimerTyped(1)
+    running = True
+
+    try:
+        while running:
+            timer += 1
+
+
+            if timer == 25 * 60:
+                print("[Timer] DONE")
+            else:
+                get_time()
+                print("[Seconds]", timer)
+
+    except NameError:
+        print("Error")
+
+asyncio.run(get_Timer())
